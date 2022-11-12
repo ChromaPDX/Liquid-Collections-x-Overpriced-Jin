e54186da80b700900c9a0dfe6ca12938ec074e69
adam wong <adamwong246@gmail.com>
2022-11-12 15:33:19 -0800

    rebuild app with style

--- index.tsx
+++ index.tsx
@@ -1,13 +1,12 @@
-// require("@rainbow-me/rainbowkit/styles.css");
-
 import React, { useEffect, useState } from "react";
 import ReactDom from "react-dom";
 
 import configs from "./config";
 
-// import {
+import "./src/OPJ/item/css/styles.css"
 
-// } from "@thirdweb-dev/react";
+/* @ts-ignore:next-line */
+import image from "./src/OPJ/images/image.jpeg";
 
 import { Checkout } from "./src/Checkout";
 import { Redeem } from "./src/Redeem";
@@ -23,6 +22,7 @@ import {
 } from "@thirdweb-dev/react";
 import { NFT } from "@thirdweb-dev/sdk";
 
+const maxNfts = 300;
 
 function Home() {
   const { contract, isLoading: contractIsLoading } = useContract(configs.contractAddress);
@@ -37,67 +37,75 @@ function Home() {
     myNfts: []
   });
 
-  useEffect(() => {
-    if (myAddress && state.stage === 0) {
-      contract?.erc721.getOwned(myAddress).then((nfts) => {
-        setState({
-          ...state,
-          stage: 1,
-          myNfts: nfts,
-        });
-      })
-    }
-  })
+  // useEffect(() => {
+  //   if (myAddress && state.stage === 0) {
+  //     contract?.erc721.getOwned(myAddress).then((nfts) => {
+  //       setState({
+  //         ...state,
+  //         stage: 1,
+  //         myNfts: nfts,
+  //       });
+  //     })
+  //   }
+  // })
 
   return (
     <div>
-      <h2>My NFTs</h2>
 
-      {(state.myNfts || []).map((nft) => (
-        <ThirdwebNftMedia
-          key={nft.metadata.id}
-          metadata={nft.metadata}
-          height={"200"}
-        />
-      ))}
+      <section className="py-5">
+        <div className="container px-4 px-lg-5 my-5">
+          <div className="row gx-4 gx-lg-5 align-items-center">
+            <div className="col-md-6">
+              <img className="card-img-top mb-5 mb-md-0"
+
+                src={image}
+
+                alt="..." />
+            </div>
+            <div className="col-md-6">
+
+              <h1 className="display-5 fw-bolder">Liquid Collections x OPJ gin</h1>
+              <div className="fs-5 mb-5">
+                <span>{configs._pricePerToken} ETH</span>
+              </div>
+              <p className="lead">This is a wholesale depot for 300 custom-made Liquid Collections x OPJ gin NFTs. The mint button below will allow the purchase of the whole lot of 300. Hope you brought your truck.</p>
+              <div className="d-flex">
+
+                <input
+                  style={
+                    {
+                      width: '4rem'
+                    }
+                  }
+                  type="number"
+                  min="1"
+                  max="maxNfts"
+                  value={state.quantity}
+                  onChange={(e) => {
+                    setState({
+                      ...state,
+                      quantity: Math.min(Number.parseInt(e.target.value), maxNfts)
+                    })
+                  }} />
 
 
+                <button
+                  className="btn btn-outline-dark flex-shrink-0" type="button"
+                  onClick={(e) => {
+                    contract?.erc721.claim(state.quantity)
+                  }}
+                >
 
-      <hr />
+                  <i className="bi-cart-fill me-1"></i>
+                  Mint {state.quantity}
+                </button>
+              </div>
 
-      <h2>Claim an NFT, if you are allowed</h2>
-      <input
-        type="number"
-        min="1"
-        max="9"
-        value={state.quantity}
-        onChange={(e) => setState({
-          ...state,
-          quantity: Number.parseInt(e.target.value)
-        })} />
-      <button onClick={(e) => {
-        contract?.erc721.claim(state.quantity)
-      }}>claim {state.quantity} NFTs</button>
-
-      <hr />
-
-      <h2>All NFTs</h2>
-      {isReadingNfts ? (
-        <p>Loading...</p>
-      ) : (
-        <div>
-          {nfts?.map((nft) => (
-            <ThirdwebNftMedia
-              key={nft.metadata.id}
-              metadata={nft.metadata}
-              height={"200"}
-            />
-          ))}
+            </div>
+          </div>
         </div>
-      )}
+      </section>
 
-
-      <hr />
     </div>
   );
 }
